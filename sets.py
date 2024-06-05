f62abb39e94a9261de3f63b2ad146ca32e81c79a
Nex <124292133+CheYoki@users.noreply.github.com>
2024-06-05 19:23:41 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,22 +1,36 @@
-#sets
+# Sets, capítulo 1: Sin orden.
 
-my_set = {}
-print(type(my_set)) #diccionario ._.xd pq está vacío, se deben poner variables para que entienda que es un set
+my_set = {} 
+print(type(my_set)) # Diccionario ._.xd 
+# Esto se debe a que está vacío.
+# Pues no hay elementos en el conjunto.
 
 my_set = {'Python', 'JavaScript', 'C++'}
-print(type(my_set)) #ahora si lo define como set
+print(type(my_set)) # Ahora si lo define como set.
 
-print(my_set) #no hay orden, repite varias veces y veras que da de formas diferentes en la consola, no importa el orden aqui
-#si pides imprimir el 0 (el primer dato) no hay sentido pq ninguno es el primero ni el ultimo
+# O también tenemos set(['Python', 'JavaScript', 'C++'])
+my_set = {'Python', 'JavaScript', 'C++'} # ¡Prácticamente lo mismo! :D
 
-my_set.add('C++')
-print(my_set) #no acepta repetidos, no va a aparecer 2 veces o más
+# El orden no importa, pues se imprime de formas diferentes cada vez.
+# Si intentas imprimir el primer elemento (el 0), no tiene sentido,
+# ya que no hay un primer ni un último elemento en un conjunto.
+print(my_set) # {'C++', 'JavaScript', 'Python'}
+
+# El orden no importa en los conjuntos, ya que son colecciones desordenadas.
+# Si intentas imprimir el primer elemento (el índice 0), no tiene sentido,
+# ya que no hay un primer ni un último elemento en un conjunto.
+print(my_set) # No se imprime nuevamente el conjunto, ya que se mantiene igual.
 
 my_set.add('C#')
-print(my_set)
+print(my_set) # El conjunto sigue siendo {'Python', 'JavaScript', 'C++'},
+              # porque 'C++' ya estaba en el conjunto.
 
+# La función difference_update() actualiza el conjunto actual restándole otro conjunto.
+# Esto significa que elimina del conjunto actual los elementos que también están en el otro conjunto.
 my_set_0 = {'Python', 'JavaScript', 'C++'}
 my_set.difference_update(my_set_0)
-print(my_set) #da la diferencia de esos dos (my_set - my_set_0) , cual es el dato diferente
 
-#fin
+print("Difference Update: Restar otro conjunto al conjunto actual")
+print(my_set) # {'C#'} ya que 'Python', 'JavaScript' y 'C++' han sido eliminados
+
+# Fin. :D
